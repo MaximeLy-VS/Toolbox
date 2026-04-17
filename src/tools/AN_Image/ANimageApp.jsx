91e73e3b293d4b1bb92041b4bee777c8a764c441
MaximeLy-VS <lyon.maxime@gmail.com>
2026-04-17 09:34:45 +0200

    Import apiKey in ANimageApp component

--- src/tools/AN_Image/ANimageApp.jsx
+++ src/tools/AN_Image/ANimageApp.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect, useRef } from 'react';
-import { copyToClipboard, CopyButton } from '../../App'; 
+import { copyToClipboard, CopyButton, apiKey } from '../../App'; 
 import {   Upload as IconUpload, Image as IconImage, Copy as IconCopy, Check as IconCheck, AlertCircle as IconAlert, Loader2 as IconLoader, Info, Table as IconTable, Layout as IconLayout, ArrowRight, Wrench, ChevronLeft, Sparkles, Wand2 as IconWand, Download, Zap, FileText as IconText,} from 'lucide-react';
 
 export default function ANimageApp() {
