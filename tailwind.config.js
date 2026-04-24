1f5fbc2c9d5ae9d32136850a8678554f0f198922
MaximeLy-VS <lyon.maxime@gmail.com>
2026-04-24 11:00:19 +0200

    Enhance Tailwind config with custom animations
    Added custom keyframes and animation for slide effect.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,21 @@ export default {
   },
   plugins: [require('@tailwindcss/typography')],
 }
+
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        'slide-up': {
+          '0%, 20%': { transform: 'translateY(0%)' },
+          '25%, 45%': { transform: 'translateY(-100%)' },
+          '50%, 70%': { transform: 'translateY(-200%)' }, // Optionnel : si tu ajoutes un 3ème mot
+          '75%, 100%': { transform: 'translateY(0%)' },
+        },
+      },
+      animation: {
+        'slide-vertical': 'slide-up 6s infinite ease-in-out',
+      },
+    },
+  },
+};
